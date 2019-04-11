32d73e6fc09ab8ec42bf255776aec01b70052e5d
N. Ben Cohen <breathevalue@icloud.com>
2019-04-10 20:33:05 -0600

    Make clean.py do something sensible. Remove all *.pyc files from repository

--- clean.py
+++ clean.py
@@ -1,6 +1,15 @@
 import os
-path = ["PoissonSolver/matvec1D.so","PoissonSolver/matvec2D.so","PoissonSolver/ps3d.so"]
-for p in path:
-  if os.path.isfile(p):
-    os.remove(p)
-print(".so files removed")
+import glob
+from shutil import rmtree
+
+globs = ["build", "**/__pycache__", "PoissonSolver/**/*.so", "PoissonSolver/**/*.c", "**/*.pyc" ]
+
+for pattern in globs:
+    for p in glob.glob(pattern, recursive=True):
+        if os.path.isfile(p):
+            print(f"Removing file '{p}''")
+            os.remove(p)
+        elif os.path.isdir(p):
+            print(f"Removing directory '{p}''")
+            rmtree(p)
+
