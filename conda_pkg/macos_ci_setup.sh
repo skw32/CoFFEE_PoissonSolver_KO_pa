bf2c8df47e0aada4b03f3e913793b597d4e3328f
N. Ben Cohen <breathevalue@icloud.com>
2019-05-11 16:06:31 -0600

    Install macos 10.9 sdk in expected location in macOS CI worker

---
+++ conda_pkg/macos_ci_setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+
+# needed for some tests
+brew install tree wget
+
+# install MacOS 10.9 sdk to /opt/MacOSX10.9.sdk
+(
+    sudo mkdir -p /opt
+    cd /opt
+    wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
+    tar -xvf MacOSX10.9.sdk.tar.xz
+    rm MacOSX10.9.sdk.tar.xz
+)
+
