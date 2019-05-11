32d0d66fc5ea62fd5e369ef2493df21e6b05f98d
N. Ben Cohen <breathevalue@icloud.com>
2019-05-11 16:17:29 -0600

    Fix permission error when installing macos 10.9 sdk in ci

--- conda_pkg/macos_ci_setup.sh
+++ conda_pkg/macos_ci_setup.sh
@@ -6,6 +6,7 @@ brew install tree wget
 # install MacOS 10.9 sdk to /opt/MacOSX10.9.sdk
 (
     sudo mkdir -p /opt
+    sudo chmod oug+w /opt
     cd /opt
     wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
     tar -xvf MacOSX10.9.sdk.tar.xz
