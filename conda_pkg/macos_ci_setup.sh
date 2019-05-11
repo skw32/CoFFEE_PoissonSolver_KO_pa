d9efdc84408dc9a18c9bde03697f5b4a1fa3aece
N. Ben Cohen <breathevalue@icloud.com>
2019-05-11 16:26:35 -0600

    Reduce verbosity of output when installing macos 10.9 SDK

--- conda_pkg/macos_ci_setup.sh
+++ conda_pkg/macos_ci_setup.sh
@@ -9,7 +9,7 @@ brew install tree wget
     sudo chmod oug+w /opt
     cd /opt
     wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
-    tar -xvf MacOSX10.9.sdk.tar.xz
+    tar -xf MacOSX10.9.sdk.tar.xz
     rm MacOSX10.9.sdk.tar.xz
 )
 
