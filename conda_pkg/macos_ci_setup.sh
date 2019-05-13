03629cdd59efda149f75621e1bf3fc02acf4523d
N. Ben Cohen <breathevalue@icloud.com>
2019-05-13 09:13:51 -0600

    Add README and document the need to install the macOS 10.9 sdk for the build to succeed

--- conda_pkg/macos_ci_setup.sh
+++ conda_pkg/macos_ci_setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash -x
 
-# needed for some tests
-brew install tree wget
+brew install wget
+
+sudo mkdir -p /opt && sudo chown $(whoami) /opt && sudo chmod u+w /opt
 
 # install MacOS 10.9 sdk to /opt/MacOSX10.9.sdk
 (
-    sudo mkdir -p /opt
-    sudo chmod oug+w /opt
     cd /opt
     wget https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
     tar -xf MacOSX10.9.sdk.tar.xz
