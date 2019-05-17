75694cb10a1509be87810d2b4c995b9cc961b2b0
Suzanne K. Wallace <suzywallace501@gmail.com>
2019-05-17 17:57:32 +0200

    Make edits to atomic_V.pyx to try to generalise atom potential alignment for interstitials

--- PoissonSolver/atomic_calculate/atomic_V.pyx
+++ PoissonSolver/atomic_calculate/atomic_V.pyx
@@ -9,8 +9,9 @@ ctypedef np.complex128_t DTYPE_t
 
 
 def atomic_3d(
+             str defect_type,
              int defect_line,
-             int atom_number,
+             int host_atom_num,
              double beta,
              long[:] grid,
              double[:,:] latvec,
@@ -22,42 +23,101 @@ def atomic_3d(
     cdef int i,j,k,l,jmax,kmax,lmax 
     cdef double[:] G = np.zeros((3))
     cdef double[:] R = np.zeros((3))
-    cdef double[:] V_atomic = np.zeros((atom_number-1))
+    if (defect_type == 'antisite' or defect_type == 'vacancy'):
+        cdef double[:] V_atomic = np.zeros((host_atom_num-1))
+    else: # include all atoms in host supercell for interstitial
+        cdef double[:] V_atomic = np.zeros((host_atom_num))
     cdef complex second 
     cdef double  third 
-    print "TZ test calculations"
-    jmax = (grid[0]-1)/2
-    kmax = (grid[1]-1)/2
-    lmax = (grid[2]-1)/2 
-    for i in range(0,defect_line): 
-        print i, " point"
-        R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
-        R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
-        R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
-        for j in range(-jmax,jmax+1):
-            for k in range(-kmax,kmax+1):
-                for l in range(-lmax,lmax+1): 
-                    G[0] = G1[j+jmax][k+kmax][l+lmax]
-                    G[1] = G2[j+jmax][k+kmax][l+lmax]
-                    G[2] = G3[j+jmax][k+kmax][l+lmax]
-                    second = np.exp(1j*np.dot(G,R))
-                    second = second*V_G[j+jmax][k+kmax][l+lmax]
-                    third = np.exp(-0.5*np.dot(G,G)*beta*beta)
-                    V_atomic[i] += np.real(second*third) 
-    for i in range(defect_line+1,atom_number): 
-        print i-1, " point"
-        R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
-        R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
-        R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
-        for j in range(-jmax,jmax+1):
-            for k in range(-kmax,kmax+1):
-                for l in range(-lmax,lmax+1): 
-                    G[0] = G1[j+jmax][k+kmax][l+lmax]
-                    G[1] = G2[j+jmax][k+kmax][l+lmax]
-                    G[2] = G3[j+jmax][k+kmax][l+lmax]
-                    second = np.exp(1j*np.dot(G,R))
-                    second = second*V_G[j+jmax][k+kmax][l+lmax]
-                    third = np.exp(-0.5*np.dot(G,G)*beta*beta)
-                    V_atomic[i-1] += np.real(second*third) 
- 
+    #print "TZ test calculations"
+
+    if (defect_type == 'vacancy' or defect_type='antisite'):
+        jmax = (grid[0]-1)/2
+        kmax = (grid[1]-1)/2
+        lmax = (grid[2]-1)/2
+
+        if (defect_line > 0 and defect_line < host_atom_num-1):
+            for i in range(0,defect_line): 
+                #print i, " point"
+                R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
+                R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
+                R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
+                for j in range(-jmax,jmax+1):
+                    for k in range(-kmax,kmax+1):
+                        for l in range(-lmax,lmax+1): 
+                            G[0] = G1[j+jmax][k+kmax][l+lmax]
+                            G[1] = G2[j+jmax][k+kmax][l+lmax]
+                            G[2] = G3[j+jmax][k+kmax][l+lmax]
+                            second = np.exp(1j*np.dot(G,R))
+                            second = second*V_G[j+jmax][k+kmax][l+lmax]
+                            third = np.exp(-0.5*np.dot(G,G)*beta*beta)
+                            V_atomic[i] += np.real(second*third) 
+            for i in range(defect_line+1,host_atom_num): 
+                #print i-1, " point"
+                R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
+                R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
+                R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
+                for j in range(-jmax,jmax+1):
+                    for k in range(-kmax,kmax+1):
+                        for l in range(-lmax,lmax+1): 
+                            G[0] = G1[j+jmax][k+kmax][l+lmax]
+                            G[1] = G2[j+jmax][k+kmax][l+lmax]
+                            G[2] = G3[j+jmax][k+kmax][l+lmax]
+                            second = np.exp(1j*np.dot(G,R))
+                            second = second*V_G[j+jmax][k+kmax][l+lmax]
+                            third = np.exp(-0.5*np.dot(G,G)*beta*beta)
+                            V_atomic[i-1] += np.real(second*third) 
+        elif (defect_line == 0):
+            for i in range(1,host_atom_num): 
+                #print i, " point"
+                R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
+                R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
+                R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
+                for j in range(-jmax,jmax+1):
+                    for k in range(-kmax,kmax+1):
+                        for l in range(-lmax,lmax+1): 
+                            G[0] = G1[j+jmax][k+kmax][l+lmax]
+                            G[1] = G2[j+jmax][k+kmax][l+lmax]
+                            G[2] = G3[j+jmax][k+kmax][l+lmax]
+                            second = np.exp(1j*np.dot(G,R))
+                            second = second*V_G[j+jmax][k+kmax][l+lmax]
+                            third = np.exp(-0.5*np.dot(G,G)*beta*beta)
+                            V_atomic[i] += np.real(second*third)
+        else: # Defect is final line of geometry file
+            for i in range(0,host_atom_num-1): 
+                #print i, " point"
+                R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
+                R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
+                R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
+                for j in range(-jmax,jmax+1):
+                    for k in range(-kmax,kmax+1):
+                        for l in range(-lmax,lmax+1): 
+                            G[0] = G1[j+jmax][k+kmax][l+lmax]
+                            G[1] = G2[j+jmax][k+kmax][l+lmax]
+                            G[2] = G3[j+jmax][k+kmax][l+lmax]
+                            second = np.exp(1j*np.dot(G,R))
+                            second = second*V_G[j+jmax][k+kmax][l+lmax]
+                            third = np.exp(-0.5*np.dot(G,G)*beta*beta)
+                            V_atomic[i] += np.real(second*third) 
+
+    else: # defect_type is interstitial, so do not skip defect_line in host supercell
+        jmax = (grid[0])/2
+        kmax = (grid[1])/2
+        lmax = (grid[2])/2 
+        for i in range(0,host_atom_num): 
+            #print i, " point"
+            R[0] = host_corr[i,0]*latvec[0,0] + host_corr[i,1]*latvec[1,0] + host_corr[i,2]*latvec[2,0]
+            R[1] = host_corr[i,0]*latvec[0,1] + host_corr[i,1]*latvec[1,1] + host_corr[i,2]*latvec[2,1]
+            R[2] = host_corr[i,0]*latvec[0,2] + host_corr[i,1]*latvec[1,2] + host_corr[i,2]*latvec[2,2]
+            for j in range(-jmax,jmax+1):
+                for k in range(-kmax,kmax+1):
+                    for l in range(-lmax,lmax+1): 
+                        G[0] = G1[j+jmax][k+kmax][l+lmax]
+                        G[1] = G2[j+jmax][k+kmax][l+lmax]
+                        G[2] = G3[j+jmax][k+kmax][l+lmax]
+                        second = np.exp(1j*np.dot(G,R))
+                        second = second*V_G[j+jmax][k+kmax][l+lmax]
+                        third = np.exp(-0.5*np.dot(G,G)*beta*beta)
+                        V_atomic[i] += np.real(second*third) 
+
     return np.array(V_atomic)
